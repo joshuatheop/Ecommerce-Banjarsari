5f591946c96dca849f122e486af72209c87a1971
Davyhilbram <davyhilbram57@gmail.com>
2026-07-03 13:39:09 +0700

    Commit 3 : Perubahan global.css menjadi globaldavy.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./globalsdavy.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 
